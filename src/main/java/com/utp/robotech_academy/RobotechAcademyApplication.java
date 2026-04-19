16b923c7638d0df8b8cef8a40cdfdbb7144949ec
Fabiana Castro <fabiana.castroc22@gmail.com>
2026-04-19 18:29:46 -0500

    First commit: Estructura inicial Spring Boot

---
+++ src/main/java/com/utp/robotech_academy/RobotechAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.utp.robotech_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotechAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotechAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/robotech_academy/RobotechAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.robotech_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotechAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
